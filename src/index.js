467775dca37fd41618f82a6d31961b5e1bbe826c
dmlaziuk <dm.laziuk@gmail.com>
2018-09-12 02:43:27 +0300

    Solve quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  const re = /(-?\d+)\s?\*\s?x\^2\s?([+-]\s?\d+)\s?\*\s?x\s?([+-]\s?\d+)/;
+  const a = Number(re.exec(equation)[1]);
+  const b = Number(re.exec(equation)[2].replace(' ', ''));
+  const c = Number(re.exec(equation)[3].replace(' ', ''));
+  const d = Math.sqrt((b ** 2) - 4 * a * c);
+  const x1 = Math.round((-b + d) / (2 * a));
+  const x2 = Math.round((-b - d) / (2 * a));
+  return [x1, x2].sort((left, right) => left - right);
+};
